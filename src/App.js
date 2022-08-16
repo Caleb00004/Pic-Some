2c66d8f06213377c1ca3999dd0897e2359d4eed1
caleb <calebakpan7@gmail.com>
2022-08-16 12:39:19 +0100

    designed header component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import Header from './components/Header'
 import './App.css';
+
+/* 
+New URL for icons
+https://cdn.jsdelivr.net/npm/remixicon@2.3.0/fonts/remixicon.css
+*/
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <h1>Home page</h1>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import './Header.css'
+
+function Header() {
+    return (
+        <header>
+            <h1>Pic Some</h1>
+            <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+        </header>
+    )
+}
+
+export default Header
